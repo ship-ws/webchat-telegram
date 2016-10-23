2153efb9d218e6850fd2bd2ae44c4d41061f68ae
Edson Celio <edsoncelio@alu.ufc.br>
2016-10-23 10:58:16 -0300

    adicionado estrutura do projeto

---
+++ config.py
@@ -0,0 +1,14 @@
+#Debug ativado
+DEBUG = True
+
+# Diretório da aplicação
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+# Definir configuração do banco
+
+
+#Threads usadas, usualmente são utilizadas 2- cores
+THREADS_PER_PAGE = 2
+
+
---
+++ run.py
@@ -0,0 +1,2 @@
+from app import app
+app.run(port=8080, debug=True)
